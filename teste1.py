ecdc2b43c570652eb1d90ac6484406ccc5bc1213
Viny Dinis <vinytech22@gmail.com>
2025-01-08 17:45:57 -0300

    Create teste1.py

---
+++ teste1.py
@@ -0,0 +1,13 @@
+nome = 'Carol'
+hobby = 'ouvir música'
+print(f'Olá sou a {nome}, e gosto de {hobby}.')
+
+# Escreva: "bateria eletrônica"
+
+b = 'ba'
+parte2 = 'nica'
+a = 'a'
+r = 'ri'
+parte1 = 'eletrô'
+t = 'te'
+print(f'{b}{t}{r}{a} {parte1}{parte2}')
